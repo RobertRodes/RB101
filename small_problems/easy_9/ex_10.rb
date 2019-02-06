da7dd577408976652baff5658f54949b66bef626
Bob Rodes <robtrodes@aol.com>
2019-02-05 22:20:08 -0600

    Small Exercises: Easy 9, ex_10.rb mod

--- small_problems/easy_9/ex_10.rb
+++ small_problems/easy_9/ex_10.rb
@@ -6,8 +6,7 @@ def buy_fruit(arr)
   arr.each_with_index { |v1, v2, a| p v1 }
 end
 
-buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) 
- #==
- # ["apples", "apples", "apples", "orange", "bananas","bananas"]
+buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) ==
+  ["apples", "apples", "apples", "orange", "bananas","bananas"]
  
 #p buy_fruit(['apples', 'oranges', 'bananas'])
